47e2a52270064836efd43d83a4b8097cf687091d
ruhulaminjr <ruhultuber@gmail.com>
2022-01-01 20:46:09 +0600

    initial setup

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient } = require("mongodb");
+const objectId = require("mongodb").ObjectId;
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+// firebase token verify setup
+const admin = require("firebase-admin");
+
+const serviceAccount = JSON.parse(process.env.FIREBASEAUTH);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+async function verifyUserToken(req, res, next) {
+  if (req.headers?.authorization?.startsWith("Bearer ")) {
+    const userToken = req.headers.authorization.split(" ")[1];
+    try {
+      const decodedUser = await admin.auth().verifyIdToken(userToken);
+      req.decodedEmail = decodedUser.email;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  next();
+}
+// middleware
+app.use(cors());
+app.use(express.json());
+// mongodb config
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bdjvz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+// mongodb connect and server api
